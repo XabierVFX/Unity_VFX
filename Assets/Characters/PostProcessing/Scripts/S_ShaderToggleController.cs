87ab10caf36ce8f11930615e42625385619763ab
Xabier <xabiermartinezdezuazo@gmail.com>
2025-01-29 10:30:50 +0100

    [VFX] Clase 11

---
+++ Assets/Characters/PostProcessing/Scripts/S_ShaderToggleController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class S_ShaderToggleController : MonoBehaviour
+{
+    public Material material; // Material del Shader
+    public FullScreenPassRendererFeature rendererFeature; // Asigna aquí tu Renderer Feature
+
+    public void EnableShader()
+    {
+        if (rendererFeature != null)
+        {
+            rendererFeature.SetActive(true);
+            Debug.Log("Shader activado.");
+        }
+    }
+
+    public void DisableShader()
+    {
+        if (rendererFeature != null)
+        {
+            rendererFeature.SetActive(false);
+            Debug.Log("Shader desactivado.");
+        }
+    }
+
+    public void SetShaderParameter(string parameterName, float value)
+    {
+        if (material != null && material.HasProperty(parameterName))
+        {
+            material.SetFloat(parameterName, value);
+            Debug.Log($"Parámetro {parameterName} establecido a {value}");
+        }
+    }
+}
